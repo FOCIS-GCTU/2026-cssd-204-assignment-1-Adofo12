60b575ca32b2c6b36d88b040a812065a4ed71789
Adofo12 <2425403290@live.gctu.edu.gh>
2026-05-30 11:28:46 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Adofo Kelvin Duah
+# STUDENT ID:  2425403290
+# Email: adofokelvin12@gmail.com
+# Grader: Augustus
+#
+# On my honor, Adofo Kelvin Duah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials AKD in stylized large block letters using each letter as its own pixel
 
+    print()
+    print("...AKD")
+    print()
+    print(".......AAAA............KK......KK.........DDDDDDD...........")
+    print("......AAAAAA...........KK.....KK..........DD....DDD.........")
+    print(".....AA....AA..........KK....KK...........DD.....DD.........")
+    print("....AA......AA.........KK...KK............DD......DD........")
+    print("....AA......AA.........KK..KK.............DD......DD........")
+    print("....AAAAAAAAAA.........KKKKKK.............DD......DD........")
+    print("....AA......AA.........KK..KK.............DD......DD........")
+    print("....AA......AA.........KK...KK............DD.....DD.........")
+    print("....AA......AA.****....KK....KK...****....DD....DDD..****...")
+    print("....AA......AA.****....KK.....KKK.****....DDDDDDD....****...")
+    print()
 
 
 main()
